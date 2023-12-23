067862368c0a3a1beae1a55e662c0863aca8ab7a
Nursel Demirkiran <demirkiran3@gmail.com>
2023-12-23 18:03:56 +0300

    events and functions

---
+++ JavaScript/JS05/script.js
@@ -0,0 +1,28 @@
+
+var name = "Nursel  Demirkiran";
+
+function showName() {
+  
+    console.log(name);
+
+}
+
+function sum(num1, num2) {
+  
+    console.log(num1 + num2);
+    
+}
+
+function writeText(text) {
+  
+    console.log("writeText method worked");
+    
+    document.write(text);
+
+}
+
+function bodyOnLoad() {
+
+  console.log("body load finished");
+
+}
