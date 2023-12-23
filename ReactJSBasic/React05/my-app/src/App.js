29b9e6339c1443e0c1a88aab8603091c64f3499f
Nursel Demirkiran <demirkiran3@gmail.com>
2023-12-24 01:08:10 +0300

    ReactJS components

---
+++ ReactJSBasic/React05/my-app/src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header.js";
+
+function App() {
+  return (
+
+    <div>
+      <Header/>
+      Hello World!
+    </div>
+
+  );
+}
+
+export default App;
---
+++ ReactJSBasic/React05/my-app/src/components/Header.js
@@ -0,0 +1,6 @@
+function Header(paams) {
+    return(
+        <h1>ReactJS Component</h1>
+    );
+}
+export default Header;
