97cb8464330394fd5c5172307978b9dacbcaa924
Nursel Demirkiran <demirkiran3@gmail.com>
2023-12-23 20:27:45 +0300

    ReactJS Basic

--- RectJSBasic/React01/index.js
+++ RectJSBasic/React01/index.js
@@ -1,7 +1,23 @@
-var slugify = require("slugify");
+//var slugify = require("slugify"); // commonjs version
+import slugify from "slugify";
+//import mymath from "./my-math.js";
+//import { sum,diff } from "./my-math.js";
+import mymath, { sum,diff,str,nmb,obj,arr } from "./my-math.js";
 
-const str = "Hello World!";
-//const result = slugify(str);
-const result = slugify(str, "=");
+// const str = "Hello World!";
+// //const result = slugify(str);
+// const result = slugify(str, "=");
+// console.log(result);
 
-console.log(result);
+// console.log(sum(4,5));
+//console.log(diff(5,2));
+
+mymath(`SUM : ${sum(4,5)}`);
+mymath(`DIFF : ${diff(5,2)}`);
+
+console.log(str);
+console.log(nmb);
+console.log(obj);
+console.log(obj.category);
+console.log(obj.order);
+console.log(arr);
---
+++ RectJSBasic/React01/my-math.js
@@ -0,0 +1,55 @@
+// function sum(a,b){
+   
+//     return a + b;
+
+// }
+
+
+// const sum = function (a, b) {
+
+//   return a + b;
+
+// };
+
+
+// const sum = (a, b) => {
+
+//   return a + b;
+
+// };
+
+
+// const sum = (a,b) => {
+   
+//     return a + b;
+
+// }
+
+export const sum = (a,b) =>  a + b;
+export const diff = (a,b) => a - b;
+//const write = (msg) => console.log(msg);
+
+export default (msg) => console.log(msg);
+
+export const str = "Hello World";
+export const nmb = 12;
+export const obj = {
+
+    category: "abc",
+    order: 10
+
+};
+
+export const arr = ["apple", "kiwi", "strawberry"];
+
+// export const sum = (a,b) =>  a + b; // export
+// export const diff = (a,b) => a - b;
+
+
+//export default sum;
+
+//export { sum, diff, write };
+
+//export default write;
+
+
