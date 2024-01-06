07ed159145f4da33128909388da0c777132ba595
Nursel Demirkiran <demirkiran3@gmail.com>
2024-01-06 14:59:03 +0300

    ReactJS Router

--- ReactJSBasic/React06/src/App.js
+++ ReactJSBasic/React06/src/App.js
@@ -1,15 +1,56 @@
+import { Link, Route, Routes } from "react-router-dom";
 import UsingAxios from "./components/UsingAxios";
 import UsingFetch from "./components/UsingFetch";
 
 function App() {
   return (
-    <div className="container">
 
-      {/* <UsingFetch /> */}
+    <div>
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
 
-      <UsingAxios />
+      <div className="container">
 
+        {/* <UsingFetch /> */}
+
+        {/* <UsingAxios /> */}
+
+      </div>
     </div>
+
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can d othis, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          The feels like an existential question, don't you think ?
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
   );
 }
 
